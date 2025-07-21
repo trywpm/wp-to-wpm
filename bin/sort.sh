d1f843a80bedce87ec0cf5a97c5aeff2d009462b
thelovekesh <lovekesh.kumar@rtcamp.com>
2025-07-21 16:58:30 +0530

    Update sort script

--- bin/sort.sh
+++ bin/sort.sh
@@ -3,15 +3,12 @@
 files=(
   plugins.json
   themes.json
-  migrated-plugins.json
-  migrated-themes.json
-  ignored-plugins.json
-  ignored-themes.json
+  conflicts.json
 )
 
 for file in "${files[@]}"; do
   if [ -f "$file" ]; then
-    cat "$file" | jq -S . > "sorted-$file" && mv "sorted-$file" "$file"
+    jq sort $file > "sorted-$file" && mv "sorted-$file" "$file"
   else
     echo "File $file does not exist, skipping."
   fi
