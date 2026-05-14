81f082f8a89467395fe2eab750a882782bd18728
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-15 02:50:14 +0530

    Add svn helpers

---
+++ pkg/svn/svn.go
@@ -0,0 +1,72 @@
+package svn
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+var (
+	httpClient = &http.Client{}
+)
+
+var (
+	hrefBytes   = []byte("href")
+	parentBytes = []byte("../")
+)
+
+func getSvnList(ctx context.Context, svnRepo string) (map[string]struct{}, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, svnRepo, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch data from %s: %w", svnRepo, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch repo %s, status code: %d", svnRepo, resp.StatusCode)
+	}
+
+	list := make(map[string]struct{})
+	z := html.NewTokenizer(resp.Body)
+
+	for {
+		switch z.Next() {
+		case html.ErrorToken:
+			err := z.Err()
+			if errors.Is(err, io.EOF) {
+				return list, nil
+			}
+			return nil, fmt.Errorf("error tokenizing html from %s: %w", svnRepo, err)
+
+		case html.StartTagToken:
+			name, hasAttr := z.TagName()
+			if !hasAttr || len(name) != 1 || name[0] != 'a' {
+				continue
+			}
+
+			for {
+				k, v, more := z.TagAttr()
+				if bytes.Equal(k, hrefBytes) {
+					if len(v) > 1 && v[len(v)-1] == '/' && !bytes.Equal(v, parentBytes) {
+						slug := v[:len(v)-1]
+						list[string(slug)] = struct{}{}
+					}
+					break
+				}
+				if !more {
+					break
+				}
+			}
+		}
+	}
+}
