4cf4b50dc8b522a95d16b30b8b5946d6e958538f
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-15 03:41:08 +0530

    Update store updater

--- cmd/update/main.go
+++ cmd/update/main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"maps"
-	"net/http"
 	"os"
-	"path/filepath"
 	"slices"
 	"strings"
 	"sync"
 	"sync/atomic"
 
+	"wpm-migration/pkg/store"
+	"wpm-migration/pkg/svn"
+	"wpm-migration/pkg/validate"
 	"wpm-migration/pkg/wporg"
 
-	"golang.org/x/net/html"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -38,12 +35,6 @@ const (
 	pluginsSvnRepo = "https://plugins.svn.wordpress.org/"
 )
 
-var (
-	hrefBytes   = []byte("href")
-	parentBytes = []byte("../")
-	httpClient  = &http.Client{}
-)
-
 type packageClosure string
 
 const (
@@ -55,148 +46,6 @@ const (
 type resolvedConfig struct {
 	Themes  []string `json:"themes"`
 	Plugins []string `json:"plugins"`
-}
-
-func isValidPackageName(name []byte) bool {
-	n := len(name)
-	if n < 3 || n > 164 {
-		return false
-	}
-
-	for i := range n {
-		c := name[i]
-
-		// check for allowed characters a-z
-		if c >= 'a' && c <= 'z' {
-			continue
-		}
-
-		// check for allowed characters 0-9
-		if c >= '0' && c <= '9' {
-			continue
-		}
-
-		// check for allowed special characters `-`
-		if c == '-' {
-			if i == 0 || i == n-1 || name[i-1] == '-' {
-				return false
-			}
-			continue
-		}
-
-		return false
-	}
-
-	return true
-}
-
-func getSvnList(ctx context.Context, svnRepo string) (map[string]struct{}, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, svnRepo, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create request: %w", err)
-	}
-
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch data from %s: %w", svnRepo, err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to fetch repo %s, status code: %d", svnRepo, resp.StatusCode)
-	}
-
-	list := make(map[string]struct{})
-	z := html.NewTokenizer(resp.Body)
-
-	for {
-		switch z.Next() {
-		case html.ErrorToken:
-			err := z.Err()
-			if errors.Is(err, io.EOF) {
-				return list, nil
-			}
-			return nil, fmt.Errorf("error tokenizing html from %s: %w", svnRepo, err)
-
-		case html.StartTagToken:
-			name, hasAttr := z.TagName()
-			if !hasAttr || len(name) != 1 || name[0] != 'a' {
-				continue
-			}
-
-			for {
-				k, v, more := z.TagAttr()
-				if bytes.Equal(k, hrefBytes) {
-					if len(v) > 1 && v[len(v)-1] == '/' && !bytes.Equal(v, parentBytes) {
-						slug := v[:len(v)-1]
-						if isValidPackageName(slug) {
-							list[string(slug)] = struct{}{}
-						}
-					}
-					break
-				}
-				if !more {
-					break
-				}
-			}
-		}
-	}
-}
-
-func readJson(filename string, dest any) error {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			return nil
-		}
-		return fmt.Errorf("failed to read file %s: %w", filename, err)
-	}
-
-	if len(data) == 0 {
-		return nil
-	}
-
-	if err := json.Unmarshal(data, dest); err != nil {
-		return fmt.Errorf("failed to unmarshal json from file %s: %w", filename, err)
-	}
-
-	return nil
-}
-
-func writeJson(path string, data any) error {
-	dir := filepath.Dir(path)
-	if dir == "" {
-		dir = "."
-	}
-
-	tmp, err := os.CreateTemp(dir, ".tmp-*.json")
-	if err != nil {
-		return fmt.Errorf("failed to create temp file in %s: %w", dir, err)
-	}
-	tmpName := tmp.Name()
-	defer os.Remove(tmpName)
-
-	encoder := json.NewEncoder(tmp)
-	encoder.SetIndent("", "  ")
-	if err := encoder.Encode(data); err != nil {
-		tmp.Close()
-		return fmt.Errorf("failed to encode json to %s: %w", tmpName, err)
-	}
-
-	if err := tmp.Sync(); err != nil {
-		tmp.Close()
-		return fmt.Errorf("failed to fsync %s: %w", tmpName, err)
-	}
-
-	if err := tmp.Close(); err != nil {
-		return fmt.Errorf("failed to close %s: %w", tmpName, err)
-	}
-
-	if err := os.Rename(tmpName, path); err != nil {
-		return fmt.Errorf("failed to rename %s -> %s: %w", tmpName, path, err)
-	}
-
-	return nil
 }
 
 func main() {
@@ -215,12 +64,12 @@ func main() {
 
 	eg.Go(func() error {
 		var err error
-		themes, err = getSvnList(svnCtx, themesSvnRepo)
+		themes, err = svn.List(svnCtx, themesSvnRepo, validate.PackageName)
 		return err
 	})
 	eg.Go(func() error {
 		var err error
-		plugins, err = getSvnList(svnCtx, pluginsSvnRepo)
+		plugins, err = svn.List(svnCtx, pluginsSvnRepo, validate.PackageName)
 		return err
 	})
 
@@ -241,7 +90,7 @@ func main() {
 	}
 
 	var resolved resolvedConfig
-	if err := readJson(resolvedJson, &resolved); err != nil {
+	if err := store.GetData(store.Resolved, &resolved); err != nil {
 		log.Fatalf("failed to read resolved config: %v", err)
 	}
 
@@ -267,13 +116,13 @@ func main() {
 	pluginsList := slices.Sorted(maps.Keys(plugins))
 	slices.Sort(conflicts)
 
-	if err := writeJson(themesJson, themesList); err != nil {
+	if err := store.SetData(store.Themes, themesList); err != nil {
 		log.Fatalf("failed to write themes json: %v", err)
 	}
-	if err := writeJson(pluginsJson, pluginsList); err != nil {
+	if err := store.SetData(store.Plugins, pluginsList); err != nil {
 		log.Fatalf("failed to write plugins json: %v", err)
 	}
-	if err := writeJson(conflictsJson, conflicts); err != nil {
+	if err := store.SetData(store.Conflicts, conflicts); err != nil {
 		log.Fatalf("failed to write conflicts json: %v", err)
 	}
 
@@ -282,10 +131,10 @@ func main() {
 	closedThemes := make(map[string]packageClosure)
 	closedPlugins := make(map[string]packageClosure)
 
-	if err := readJson(closedThemesJson, &closedThemes); err != nil {
+	if err := store.GetData(store.ClosedThemes, &closedThemes); err != nil {
 		log.Printf("warning: failed to read closed themes json: %v", err)
 	}
-	if err := readJson(closedPluginsJson, &closedPlugins); err != nil {
+	if err := store.GetData(store.ClosedPlugins, &closedPlugins); err != nil {
 		log.Printf("warning: failed to read closed plugins json: %v", err)
 	}
 
@@ -410,10 +259,10 @@ func main() {
 		fmt.Println()
 	}
 
-	if err := writeJson(closedThemesJson, closedThemes); err != nil {
+	if err := store.SetData(store.ClosedThemes, closedThemes); err != nil {
 		log.Fatalf("failed to write closed themes json: %v", err)
 	}
-	if err := writeJson(closedPluginsJson, closedPlugins); err != nil {
+	if err := store.SetData(store.ClosedPlugins, closedPlugins); err != nil {
 		log.Fatalf("failed to write closed plugins json: %v", err)
 	}
 
